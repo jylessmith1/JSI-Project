4daa21894adb67485504f7fad2ff451d61456ed5
Shineka Solomon <neka.solomon@gmail.com>
2023-04-04 15:34:43 -0400

    adds the how to use this app information

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ startBtn.addEventListener('click', (e) => {
     startArea.classList.toggle('hide')
     welcomeArea.insertAdjacentHTML('afterbegin', `<article class="welcome-content">
     <h>Welcome to Kick-it!</h>
-    <p>Here's how to use your new bucketlist planner:</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam eos placeat modi nesciunt, quo eveniet porro consectetur laudantium.</p>
+    <p>Here's how to use your new Buck-It! list planner:</p>
+    <p>This is an easy way to help you come up with great ideas for fun and exciting things to before you...ya know....kick the bucket!  Click on the ____button to generate a idea.  If your don't like the idea, you can "Can-it!", if you love the idea you can "Plan-it!".  This app will aid you in your planning process, from setting your time frame, budgeting, list which friend to bring alone (or which ones to leave behide!)</p>
     </article>`);
 
 })
