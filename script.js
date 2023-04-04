9289ca1c1ca36e7cfaf19bd1074eb1820053e598
Ian <ip240z@protonmail.com>
2023-04-04 15:45:21 -0400

    Clarified some semantic classes and id. Added a get started button after the welcome button. Main area now ready for api section usage upon get started button

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let startArea = document.querySelector('.start-area');
-let startBtn = document.querySelector('#start-button');
-let welcomeArea = document.querySelector('.welcome-area')
+let welcomeBtn = document.querySelector('#welcome-button');
+let welcomeArea = document.querySelector('.main-area');
 
 
 
-
-
-startBtn.addEventListener('click', (e) => {
+welcomeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('Clicked')
     startArea.classList.toggle('hide')
-    welcomeArea.insertAdjacentHTML('afterbegin', `<article class="welcome-content">
+    
+    welcomeArea.insertAdjacentHTML('afterbegin', 
+    `<article class="welcome-content">
     <h>Welcome to Kick-it!</h>
     <p>Here's how to use your new bucketlist planner:</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam eos placeat modi nesciunt, quo eveniet porro consectetur laudantium.</p>
+    <button id="start-button">Got it, let's get started</button>
     </article>`);
-
+    
+    let startBtn = document.querySelector('#start-button');
+    startBtn.addEventListener('click', () => {
+        welcomeArea.innerHTML = '';
+    });
 })
